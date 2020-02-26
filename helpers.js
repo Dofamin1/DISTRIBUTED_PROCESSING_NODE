74ef627a6b30b596f7db2f7e79766bb15bfa6aac
Anton Kovalenko <user@Admins-MacBook-Pro.local>
2020-02-26 18:29:49 +0200

    add log functuon to helpers

--- helpers.js
+++ helpers.js
@@ -2,5 +2,9 @@ module.exports = {
     errorHandler(err) {
         console.log(err);
         throw new Error(err.message || 'unknown error')
+    },
+    log(message) {
+        const { LOGGER } = process.env;
+        if(LOGGER) console.log(message)
     }
 }
