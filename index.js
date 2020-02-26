b6a9187b0b17f8670fc26d23b5f548316e413cbd
Anton_Kovalenko <gugenot990@gmail.com>
2020-02-26 22:06:37 +0200

    linter changes + add uuid generation

--- index.js
+++ index.js
@@ -1,61 +1,101 @@
+import { v4 as uuidv4 } from "uuid";
 const cote = require("cote");
 const WorkerEventsController = require("./eventsController/workerEventsController");
 const MasterEventsController = require("./eventsController/masterEventsController");
-const { errorHandler } = require('./helpers')
-const masterBusinessLogic = require("./businessLogic/master");
-const workerBusinessLogic = require("./businessLogic/worker");
-const QueueController = require('./queueController');
-const orchestratorResponder = new cote.Responder({ name: 'Orchestrator Responder' });
+const { errorHandler } = require("./helpers");
+const MasterBusinessLogic = require("./businessLogic/master");
+const WorkerBusinessLogic = require("./businessLogic/worker");
+const QueueController = require("./queueController");
+const orchestratorResponder = new cote.Responder({
+  name: "Orchestrator Responder"
+});
 
-const { FIRST_START_NODE_STASTUS } = process.env
+const { FIRST_START_NODE_STASTUS } = process.env;
 
-class Worker {
+class BaseNode {
   constructor() {
+    this.uuid;
+    this._generateUuid();
+  }
+
+  _generateUuid() {
+    if (this.uuid) {
+      this.uuid = uuidv4();
+    }
+  }
+}
+
+class Worker extends BaseNode {
+  constructor() {
+    super();
     this.eventsController = new WorkerEventsController();
-    this.businessLogic = workerBusinessLogic;
+    this.businessLogic = WorkerBusinessLogic;
     this._setMasterResponders();
   }
 
   _setMasterResponders() {
-    const events = [{
-      eventName: 'task',
-      callback: this.businessLogic.executeTask
-    }]
+    const events = [
+      {
+        eventName: "task",
+        callback: this.businessLogic.executeTask
+      }
+    ];
 
     events.forEach(({ eventName, callback }) => {
       this.eventsController.subscribeToEvent({ eventName, callback });
-    })
+    });
   }
 }
 
-class Master {
+class Master extends BaseNode {
   constructor() {
-    this.orchestratorSubscriber = new cote.Subscriber({ name: 'Master Orchestrantor Subscriber', subscribesTo: ['task'] });
+    super();
+    this.orchestratorSubscriber = new cote.Subscriber({
+      name: "Master Orchestrantor Subscriber",
+      subscribesTo: ["task"]
+    });
     this.eventsController = new MasterEventsController();
-    this.businessLogic = masterBusinessLogic;
+    this.businessLogic = MasterBusinessLogic;
     this.queueController = QueueController;
     this._setOrchestratorSubscribers();
   }
 
   _setOrchestratorSubscribers() {
-    this.orchestratorSubscriber.on('task', this.queueController.addToQueue.bind(this.queueController));
+    this.orchestratorSubscriber.on(
+      "task",
+      this.queueController.addToQueue.bind(this.queueController)
+    );
   }
-
 }
 
-//TODO: this message should be recived by specified node, not  by all nodes
-// orchestratorResponder.on("nodeStatus", (err, responseCallback) => {
-//   errorHandler(err);
-//   if(status == 'master') {
-//     node = new Master(orchestratorResponder );
-//   } else {
-//     node = new Worker(orchestratorResponder);
-//   }
-// });
+let node =
+  FIRST_START_NODE_STASTUS == "master"
+    ? new Master(orchestratorResponder)
+    : new Worker(orchestratorResponder);
 
-orchestratorResponder.on('amIAlive', (err, responseCallback) => {
-  errorHandler(err);
-  responseCallback('i am alive')
-})
+orchestratorResponder.on("nodeStatus", (req, responseCallback) => {
+  try {
+    errorHandler(err);
 
-let node = FIRST_START_NODE_STASTUS == 'master' ? new Master(orchestratorResponder) : new Worker(orchestratorResponder); 
+    if (req.value == "master") {
+      node = new Master(orchestratorResponder);
+    } else {
+      node = new Worker(orchestratorResponder);
+    }
+
+    responseCallback({
+      uuid: node.uuid
+    });
+  } catch (err) {
+    responseCallback({
+      error: `Error happen while trying to set node ${node.uuid} master role`
+    });
+  }
+});
+
+orchestratorResponder.on("amIAlive", (req, responseCallback) => {
+  responseCallback({
+    role: node instanceof Master ? "master" : "worker",
+    uuid: node.uuid
+  });
+});
