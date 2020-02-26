1bd963f1893d9d454845591bc6a731f52b9de51d
Anton_Kovalenko <gugenot990@gmail.com>
2020-02-26 22:04:43 +0200

    linter fix

--- helpers.js
+++ helpers.js
@@ -1,10 +1,10 @@
 module.exports = {
-    errorHandler(err) {
-        console.log(err);
-        throw new Error(err.message || 'unknown error')
-    },
-    log(message) {
-        const { LOGGER } = process.env;
-        if(LOGGER) console.log(message)
-    }
-}
+  errorHandler(err) {
+    console.log(err);
+    throw new Error(err.message || "unknown error");
+  },
+  log(message) {
+    const { LOGGER } = process.env;
+    if (LOGGER) console.log(message);
+  }
+};
