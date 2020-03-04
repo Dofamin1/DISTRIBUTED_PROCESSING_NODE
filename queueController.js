3c2d1f08b2ed9d76da3c704ece5c938e4cfbce21
Anton Kovalenko <user@Admins-MacBook-Pro.local>
2020-03-04 19:32:45 +0200

    create functionality to fetch tasks

--- queueController.js
+++ queueController.js
@@ -1,8 +1,11 @@
 const Joi = require("joi");
 const  { errorHandler, log } = require('./helpers')
 class QueueController {
-  constructor() {
+  constructor(dbClient) {
     this.tasksQueue = [];
+    this.dbClient = dbClient;
+    this.TASKS_PER_ONE_FETCH = 30;
+    this.MINIMAL_TASKS_IN_QUEUE = his.TASKS_PER_ONE_FETCH * 0.3 / 100 //30%
   }
 
   addToQueue(task) {
@@ -11,11 +14,28 @@ class QueueController {
     log('TASK WAS ADDED TO QUEUE')
   }
 
-  popFromQueue() {
+  async _fetchTasks() {
+    try{
+      const tasks = await this.dbClient.fetchTasks(this.TASKS_PER_ONE_FETCH);
+      this.tasksQueue = [...this.tasksQueue, ...tasks];
+    }catch(err) {
+      console.log('ERROR FETCHING TASKS')
+      errorHandler()
+    }
+  }
+
+  async popFromQueue() {
+
+    if(this.tasksQueue.length < this.MINIMAL_TASKS_IN_QUEUE ) {
+      this._fetchTasks()
+    }
+
+
     if (!this.tasksQueue.length) {
       log("ATTENTION: the queue is empty")
-      return null;
+      await this._fetchTasks();
     }
+
     return this.tasksQueue.pop();
   }
 
